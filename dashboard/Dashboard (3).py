bd9aa1ae010e8b755d63fdf622831156ce40a606
ferdywings <116781668+ferdywings@users.noreply.github.com>
2025-03-08 15:27:13 +0700

    Update Dashboard (3).py

--- dashboard/Dashboard (3).py
+++ dashboard/Dashboard (3).py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import gdown
 
 # ID file Google Drive
 file_id = '16RkfDaAKoZ97H3HshaAQvLOXaKlwAkVI'
